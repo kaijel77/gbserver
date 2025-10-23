630405ca50afff678f217dc783d6017aa97eca0d
kaijel77 <kaijel77@gmail.com>
2025-10-23 10:54:28 +0900

    노드 게임서버 연동 유니티 클라이언트

---
+++ tgclient/Assets/scripts/AESUtil.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+
+public static class AESUtil
+{
+    public static string key = "7e510acb6ee42e08ee5a13a3e5906c7b"; // 32바이트
+    public static string ivs = "1234567890123456";                 // 16바이트
+    public static int iv_size = 16;
+
+    // AES256 CBC 암호화
+    public static string Encrypt(string plainText)
+    {
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+        // 🔹 1️⃣ 랜덤 IV 생성 (16 bytes)
+        byte[] ivBytes = new byte[16];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(ivBytes);
+        }
+
+        byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+
+        using (Aes aes = Aes.Create())
+        {
+            aes.KeySize = 256; // AES-256
+            aes.BlockSize = 128;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            aes.Key = keyBytes;
+            aes.IV = ivBytes;
+
+            using (var encryptor = aes.CreateEncryptor())
+            {
+                byte[] encrypted = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+                return Convert.ToBase64String(encrypted); // Base64 인코딩
+            }
+        }
+    }
+
+    // AES256 CBC 복호화
+    public static string Decrypt(string cipherTextBase64)
+    {
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+        byte[] cipherBytes = Convert.FromBase64String(cipherTextBase64);
+
+
+        int length = Math.Min(iv_size, cipherBytes.Length);
+        byte[] partBytes = new byte[length];
+        Array.Copy(cipherBytes, 0, partBytes, 0, length);
+
+        // Base64로 인코딩해서 문자열로 변환
+        string ivtext = Convert.ToBase64String(partBytes);
+        byte[] ivBytes = Encoding.UTF8.GetBytes(ivtext);
+
+        using (Aes aes = Aes.Create())
+        {
+            aes.KeySize = 256;
+            aes.BlockSize = 128;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            aes.Key = keyBytes;
+            aes.IV = ivBytes;
+
+            using (var decryptor = aes.CreateDecryptor())
+            {
+                byte[] decrypted = decryptor.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
+                return Encoding.UTF8.GetString(decrypted);
+            }
+        }
+    }
+}
---
+++ tgclient/Assets/scripts/AuthClient.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Windows;
+
+
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+
+public class AuthClient : MonoBehaviour
+{
+    private string baseUrl = "http://localhost:4700";
+    private string jwtToken;
+    private Data resultData;
+
+    void Start()
+    {
+        StartCoroutine(connect("serwer"));
+        //StartCoroutine(Register("serwer", "1234"));
+        //StartCoroutine(Login("user01", "1234"));
+    }
+
+    IEnumerator connect(string username)
+    {
+        yield return SendRequest("/connect", "", "POST", username, null);
+
+        if (resultData.isNickSetting == false) {
+            Debug.Log($"StartCoroutine(gameNickName())");
+            StartCoroutine(gameNickName());
+        }
+        else {
+            Debug.Log($"StartCoroutine(gameStart())");
+            StartCoroutine(gameStart());
+        }
+    }
+
+    IEnumerator gameNickName()
+    {
+        var data = JsonUtility.ToJson(new gamecharname("zkkkwkwk"));
+        string encyData = AESUtil.Encrypt(data);
+        WWWForm form = new WWWForm();
+        form.AddField("crypt", encyData);
+        yield return SendRequest("/game/gameNickCreate", "", "POST", jwtToken, form);
+    }
+
+    IEnumerator gameStart()
+    {
+        yield return SendRequest("/game/startGame", "", "POST", jwtToken, null);
+    }
+
+    IEnumerator Register(string username, string password)
+    {
+        var body = JsonUtility.ToJson(new UserData(username, password));
+        yield return SendRequest("/register", body, "POST", "", null);
+    }
+
+    IEnumerator Login(string username, string password)
+    {
+        var body = JsonUtility.ToJson(new UserData(username, password));
+        yield return SendRequest("/login", body, "POST", "", null);
+
+        if (!string.IsNullOrEmpty(jwtToken))
+            StartCoroutine(GetProfile());
+    }
+    IEnumerator GetProfile()
+    {
+        yield return SendRequest("/profile", null, "GET", "", null);
+    }
+
+    IEnumerator SendRequest(string endpoint, string json, string method, string header, WWWForm form)
+    {
+        UnityWebRequest www;
+        if (method == "POST")
+        {
+            if (form == null)
+            {
+                Debug.Log($"🔑 form off:  baseUrl + endpoint");
+                www = new UnityWebRequest(baseUrl + endpoint, "POST");
+
+                byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(json);
+                www.uploadHandler = new UploadHandlerRaw(bodyRaw);
+                www.downloadHandler = new DownloadHandlerBuffer();
+            }
+            else
+            {
+                Debug.Log($"🔑 form on: baseUrl + endpoint");
+                www = UnityWebRequest.Post(baseUrl + endpoint, form);
+            }
+
+            if (endpoint == "/connect")
+            {
+                www.SetRequestHeader("user-id", header);
+            }
+            else if (endpoint == "/game/gameNickCreate")
+            {
+                www.SetRequestHeader("td-access-token", header);
+            }
+            else if (endpoint == "/game/startGame")
+            {
+                www.SetRequestHeader("td-access-token", header);
+            }
+        }
+        else
+        {
+            www = UnityWebRequest.Get(baseUrl + endpoint);  
+        }
+
+//        if (!string.IsNullOrEmpty(jwtToken))
+//            www.SetRequestHeader("Authorization", "Bearer " + jwtToken);
+
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log($"✅ {endpoint} 응답: {www.downloadHandler.text}");
+            if (endpoint == "/connect")
+            {
+                var result = JsonUtility.FromJson<Login2Response>(www.downloadHandler.text);
+                Debug.Log($"🔑 Data 토큰: {result.data}");
+                resultData = result.data;
+                Debug.Log($"🔑 data2 토큰: {result.data.authToken}");
+                jwtToken = result.data.authToken;
+                Debug.Log($"🔑 JWT 토큰: {jwtToken}");
+            }
+        }
+        else
+        {
+            Debug.LogError($"❌ 요청 실패 ({endpoint}): {www.error}");
+        }
+    }
+
+    [System.Serializable]
+    public class UserData
+    {
+        public string username;
+        public string password;
+        public UserData(string u, string p)
+        {
+            username = u;
+            password = p;
+        }
+    }
+
+    [System.Serializable]
+    public class cryptData
+    {
+        public string crypt;
+        public cryptData(string u)
+        {
+            crypt = u;
+        }
+    }
+
+
+    [System.Serializable]
+    public class gamecharname
+    {
+        public string gamechar_name;
+        public gamecharname(string u)
+        {
+            gamechar_name = u;
+        }
+    }
+
+    [System.Serializable]
+    public class LoginResponse
+    {
+        public bool success;
+        public string authToken;
+    }
+
+    [System.Serializable]
+    public class UserInfo
+    {
+        public int accountno;
+        public string user_id;
+        public string password;
+    }
+
+    [System.Serializable]
+    public class Data
+    {
+        public string server_time;
+        public bool isCreateAccount;
+        public bool isNickSetting;
+        public string authToken;
+        public UserInfo userInfo;
+        // serverAddress가 빈 객체 {} 이므로, 임시로 string 처리 가능
+        public string serverAddress;
+    }
+
+    [System.Serializable]
+    public class Login2Response
+    {
+        public int errorCode;
+        public bool isSuccess;
+        public Data data;
+    }
+
+
+}
