4e2788ed3c679d1a1068d331b0b2a49be388c6fd
ohtg <kaijel77@gmail.com>
2025-11-26 13:43:56 +0900

    미션 관련 API 기본 작업완료

--- tgserver/class/missionClass.js
+++ tgserver/class/missionClass.js
@@ -20,7 +20,7 @@ class missionClass  extends baseClass {
             this.includeHandler(['mysqlHandler']);
 
             let missionList = [];
-            let select = `mission_no, mission_id, mission_type, mission_count, create_date`;
+            let select = `mission_id, mission_type, mission_count, create_date`;
             const query = `SELECT ${select} FROM tbl_missionInfo WHERE account_no='${account_no}'`;
 
             await this.mysqlHandlerClass
@@ -30,7 +30,6 @@ class missionClass  extends baseClass {
                     for(let mission_info of result)
                     {
                         missionList.push({
-                            mission_no: mission_info.mission_no,
                             mission_id: mission_info.mission_id,
                             mission_type: mission_info.mission_type,
                             mission_count: mission_info.mission_count,
@@ -56,13 +55,13 @@ class missionClass  extends baseClass {
     // @param mission_no 미션 아이디
     // @returns {Promise<*[]|*>} 미션 정보
     // 
-    async getMissionInfo (account_no, mission_no) {
+    async getMissionInfo (account_no, mission_id) {
        try {
             this.includeHandler(['mysqlHandler']);
 
             let missionInfo = null;
-            let select = `mission_no, mission_id, mission_type, mission_count, create_date`;
-            const query = `SELECT ${select} FROM tbl_missionInfo WHERE account_no='${account_no}' and mission_no='${mission_no}'`;
+            let select = `mission_id, mission_type, mission_count, create_date`;
+            const query = `SELECT ${select} FROM tbl_missionInfo WHERE account_no='${account_no}' and mission_id='${mission_id}'`;
 
             await this.mysqlHandlerClass
             .query(CONSTANT.DB.GAME, query)
@@ -70,7 +69,6 @@ class missionClass  extends baseClass {
                 if (result.length > 0) {
                     result = result[0];
                     missionInfo = {
-                        mission_no: result.mission_no,
                         mission_id: result.mission_id,
                         mission_type: result.mission_type,
                         mission_count: result.mission_count,
@@ -86,6 +84,173 @@ class missionClass  extends baseClass {
             throw err;
         }
     }
+
+
+
+
+    ///////////////////////////////////////////////////////////////
+    //
+    // 미션 정보 갱신
+    // @param account_no 계정 기본 아이디
+    // @param mission_id 미션 미션
+    // @param mission_count 미션 갯수
+    // @returns {Promise<*[]|*>} 미션 정보
+    // 
+    async addSettingMissionInfo(account_no, mission_id, mission_count) {
+        try {
+            this.includeHandler(['mysqlHandler']);
+
+            let bCreate = false;
+
+            let columns = 'account_no, mission_id, mission_type, mission_subtype, mission_status, mission_count, mission_curcount, mission_reward';
+            let values = `'${account_no}', '${mission_id}', 1, 1, 1, '${mission_count}', '${mission_count}', 0`;
+
+            let query = `INSERT INTO tbl_missionInfo (${columns}) VALUES (${values})`;
+            
+            await this.mysqlHandlerClass
+            .query(CONSTANT.DB.GAME, query)
+            .then(async (result) => {
+                if (result.affectedRows > 0) {
+                    bCreate = true;
+                }
+            })
+            .catch((err) => {
+                throw err;
+            });
+            return bCreate;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+
+    ///////////////////////////////////////////////////////////////
+    //
+    // 미션 정보 갱신
+    // @param account_no 계정 기본 아이디
+    // @param mission_no 미션 아이디
+    // @param mission_count 미션 갯수
+    // @returns {Promise<*[]|*>} 미션 정보
+    // 
+    async updateSettingMissionInfo (account_no, mission_id, mission_count) {
+        try {
+            this.includeHandler(['mysqlHandler']);
+
+            let bUpdate = false;
+            let set = `mission_count = "${mission_count}"`;
+            const query = `UPDATE tbl_missionInfo SET ${set} WHERE account_no='${account_no}' and mission_id='${mission_id}'`;
+
+            await this.mysqlHandlerClass
+            .query(CONSTANT.DB.GAME, query)
+            .then(async (result) => {
+                if (result.affectedRows > 0) {
+                    bUpdate = true;
+                }
+            })
+            .catch((err) => {
+                throw err;
+            });
+            return bUpdate;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+
+    ///////////////////////////////////////////////////////////////
+    //
+    // 미션 정보 갱신
+    // @param account_no 계정 기본 아이디
+    // @param mission_no 미션 아이디
+    // @param mission_count 미션 갯수
+    // @returns {Promise<*[]|*>} 미션 정보
+    // 
+    async completeSettingMissionInfo (account_no, mission_id) {
+        try {
+            this.includeHandler(['mysqlHandler']);
+
+            let bUpdate = false;
+            let set = `mission_status = 2`;
+            const query = `UPDATE tbl_missionInfo SET ${set} WHERE account_no='${account_no}' and mission_id='${mission_id}'`;
+
+            await this.mysqlHandlerClass
+            .query(CONSTANT.DB.GAME, query)
+            .then(async (result) => {
+                if (result.affectedRows > 0) {
+                    bUpdate = true;
+                }
+            })
+            .catch((err) => {
+                throw err;
+            });
+            return bUpdate;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+
+    ///////////////////////////////////////////////////////////////
+    //
+    // 미션 정보 갱신
+    // @param account_no 계정 기본 아이디
+    // @param mission_no 미션 아이디
+    // @param mission_count 미션 갯수
+    // @returns {Promise<*[]|*>} 미션 정보
+    // 
+    async rewardSettingMissionInfo (account_no, mission_id) {
+        try {
+            this.includeHandler(['mysqlHandler']);
+
+            let bUpdate = false;
+            let set = `mission_reward = 2`;
+            const query = `UPDATE tbl_missionInfo SET ${set} WHERE account_no='${account_no}' and mission_id='${mission_id}'`;
+
+            await this.mysqlHandlerClass
+            .query(CONSTANT.DB.GAME, query)
+            .then(async (result) => {
+                if (result.affectedRows > 0) {
+                    bUpdate = true;
+                }
+            })
+            .catch((err) => {
+                throw err;
+            });
+            return bUpdate;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////
+    //
+    // 미션 정보 갱신
+    // @param account_no 계정 기본 아이디
+    // @param mission_id 미션 아이디
+    // @returns {Promise<*[]|*>} 미션 정보
+    // 
+    async removeMissionInfo(account_no, mission_id) {
+        try {
+            this.includeHandler(['mysqlHandler']);
+
+            let bDelete = false;
+            let query = `DELETE FROM tbl_missionInfo WHERE account_no='${account_no}' and mission_id='${mission_id}'`;
+            
+            await this.mysqlHandlerClass
+            .query(CONSTANT.DB.GAME, query)
+            .then(async (result) => {
+                if (result.affectedRows > 0) {
+                    bDelete = true;
+                }
+            })
+            .catch((err) => {
+                throw err;
+            });
+            return bDelete;
+        } catch (err) {
+            throw err;
+        }
+    }    
 }
 
 module.exports = new missionClass();
--- tgserver/routes/missionManager.js
+++ tgserver/routes/missionManager.js
@@ -73,15 +73,15 @@ router.post('/missionUpdate', pscHandler.asyncWrap(async function (req, res) {
     let mission_info = await missionClass.getMissionInfo(account_info.account_no, mission_id, mission_count);
     if(mission_info === null || mission_info === undefined ){
 
-        mission_info = await missionClass.addMissionInfo(account_info.account_no, mission_id);
-        if(mission_info === null || mission_info === undefined ){
+        let bCreate = await missionClass.addSettingMissionInfo(account_info.account_no, mission_id, mission_count);
+        if(bCreate === null || bCreate === undefined || bCreate === false ){
             errorHandler.throwError(5001, 9000136); // 아이템 사용 갯수를 잘못 입력한경우
         }        
     }
     else {
 
-        let bUpdate = await missionClass.updateMiissionInfo(account_info.account_no, mission_id, mission_count);
-        if (bUpdate === false) {
+        let bUpdate = await missionClass.updateSettingMissionInfo(account_info.account_no, mission_id, mission_count);
+        if (bUpdate === null || bUpdate === undefined || bUpdate === false) {
             errorHandler.throwError(5001, 9000136); // 아이템 사용 갯수를 잘못 입력한경우
         }
     }
@@ -109,31 +109,23 @@ router.post('/missionComplete', pscHandler.asyncWrap(async function (req, res) {
 
     let account_info = req.account_info;
 
-    let params = pscHandler.verifyParams(req, ['mission_id', 'mission_count']);
+    let params = pscHandler.verifyParams(req, ['mission_id']);
     let mission_id = params['mission_id'];
-    let mission_count = params['mission_count'];
 
     if(mission_id === null || mission_id === undefined){
         // 닉네임이 있어서 실패 
         errorHandler.throwError(1099, 9000006); // 아이템 번호가 없을경우.
     }
 
-    let mission_info = await missionClass.getMissionInfo(account_info.account_no, mission_id, mission_count);
+    let mission_info = await missionClass.getMissionInfo(account_info.account_no, mission_id);
     if(mission_info === null || mission_info === undefined ){
-
-        mission_info = await missionClass.addMissionInfo(account_info.account_no, mission_id);
-        if(mission_info === null || mission_info === undefined ){
-            errorHandler.throwError(5001, 9000136); // 아이템 사용 갯수를 잘못 입력한경우
-        }        
-    }
-    else {
-
-        let bUpdate = await missionClass.updateMiissionInfo(account_info.account_no, mission_id, mission_count);
-        if (bUpdate === false) {
-            errorHandler.throwError(5001, 9000136); // 아이템 사용 갯수를 잘못 입력한경우
-        }
+        errorHandler.throwError(5001, 9000136); // 아이템 사용 갯수를 잘못 입력한경우
     }
 
+    let bUpdate = await missionClass.completeSettingMissionInfo(account_info.account_no, mission_id);
+    if (bUpdate === false) {
+        errorHandler.throwError(5001, 9000136); // 아이템 사용 갯수를 잘못 입력한경우
+    }
 
     mission_info = await missionClass.getMissionInfo(account_info.account_no, mission_id);
     if(mission_info === null || mission_info === undefined){
@@ -158,31 +150,23 @@ router.post('/missionReward', pscHandler.asyncWrap(async function (req, res) {
 
     let account_info = req.account_info;
 
-    let params = pscHandler.verifyParams(req, ['mission_id', 'mission_count']);
+    let params = pscHandler.verifyParams(req, ['mission_id']);
     let mission_id = params['mission_id'];
-    let mission_count = params['mission_count'];
 
     if(mission_id === null || mission_id === undefined){
         // 닉네임이 있어서 실패 
         errorHandler.throwError(1099, 9000006); // 아이템 번호가 없을경우.
     }
 
-    let mission_info = await missionClass.getMissionInfo(account_info.account_no, mission_id, mission_count);
+    let mission_info = await missionClass.getMissionInfo(account_info.account_no, mission_id);
     if(mission_info === null || mission_info === undefined ){
-
-        mission_info = await missionClass.addMissionInfo(account_info.account_no, mission_id);
-        if(mission_info === null || mission_info === undefined ){
-            errorHandler.throwError(5001, 9000136); // 아이템 사용 갯수를 잘못 입력한경우
-        }        
-    }
-    else {
-
-        let bUpdate = await missionClass.updateMiissionInfo(account_info.account_no, mission_id, mission_count);
-        if (bUpdate === false) {
-            errorHandler.throwError(5001, 9000136); // 아이템 사용 갯수를 잘못 입력한경우
-        }
+        errorHandler.throwError(5001, 9000136); // 아이템 사용 갯수를 잘못 입력한경우
     }
 
+    let bUpdate = await missionClass.rewardSettingMissionInfo(account_info.account_no, mission_id);
+    if (bUpdate === false) {
+        errorHandler.throwError(5001, 9000136); // 아이템 사용 갯수를 잘못 입력한경우
+    }
 
     mission_info = await missionClass.getMissionInfo(account_info.account_no, mission_id);
     if(mission_info === null || mission_info === undefined){
@@ -207,22 +191,22 @@ router.post('/missionRemove', pscHandler.asyncWrap(async function (req, res) {
 
     let account_info = req.account_info;
 
-    let params = pscHandler.verifyParams(req, ['mission_type']);
-    let mission_type = params['mission_type'];
+    let params = pscHandler.verifyParams(req, ['mission_id']);
+    let mission_id = params['mission_id'];
 
-    if(mission_type === null || mission_type === undefined){
+    if(mission_id === null || mission_id === undefined){
         // 닉네임이 있어서 실패 
         errorHandler.throwError(1099, 9000006); // 아이템 번호가 없을경우.
     }
 
-    let mission_info = await missionClass.getDeckInfo(account_info.account_no, mission_type);
+    let mission_info = await missionClass.getMissionInfo(account_info.account_no, mission_id);
     if(mission_info === null || mission_info === undefined){
         // 닉네임이 있어서 실패 
         errorHandler.throwError(1099, 9000006); // 계정생성이 실패하였습니다.
     }    
 
 
-    let bDelete = await missionClass.removeDeckInfo(account_info.account_no, mission_type);
+    let bDelete = await missionClass.removeMissionInfo(account_info.account_no, mission_id);
     if(bDelete === null || bDelete === undefined){
         // 닉네임이 있어서 실패 
         errorHandler.throwError(1099, 9000006); // 계정생성이 실패하였습니다.
